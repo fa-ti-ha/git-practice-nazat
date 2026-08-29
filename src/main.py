4d3b684bb91d073ca17729d1ee3e54c6d9b65a96
fa-ti-ha <nazat2207070@stud.kuet.ac.bd>
2026-08-29 23:51:12 +0600

    improved main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from datetime import date
 from utils import add, subtract, multiply, divide
 print("Name: Fatihatun Nazat")
 print("Today's date:", date.today())
-
+print("Arithmetic operations :")
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
 print("Multiplication:", multiply(10, 5))
