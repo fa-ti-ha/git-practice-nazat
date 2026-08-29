ec7d30215a964d55148a599d72ba72f49672ce57
fa-ti-ha <nazat2207070@stud.kuet.ac.bd>
2026-08-29 23:49:24 +0600

    Add division error handling

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from datetime import date
-from utils import add, subtract
-
+from utils import add, subtract, multiply, divide
 print("Name: Fatihatun Nazat")
 print("Today's date:", date.today())
 
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
+try:
+    print("Division:", divide(10, 2))
+except ValueError as e:
+    print("Error:", e)
+try:
+    print("Division:", divide(10, 0))
+except ValueError as e:
+    print("Error:", e)
