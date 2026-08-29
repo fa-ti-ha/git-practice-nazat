6330f9f4325e569b9b8f834c3e6c5fba6fec113e
fa-ti-ha <nazat2207070@stud.kuet.ac.bd>
2026-08-29 23:36:15 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from datetime import date
+from utils import add, subtract
 
-print("Name: Fatihatun Nazat ")
+print("Name: Fatihatun Nazat")
 print("Today's date:", date.today())
 
-
- 
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
